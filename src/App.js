0cfeda4ed2a79222f403625e0b680ea099b3f767
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-11-05 13:53:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Warehouse List</h1>
+ <h1>WAREHOUSE LIST</h1>
       <Routes>
         <Route path='/' element={<Home  />}/>
         <Route path='/details/:id' element={<Details/>}/>
